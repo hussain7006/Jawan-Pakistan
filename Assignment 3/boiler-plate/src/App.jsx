b95b0f3ba63096e011042a3d511b2d26d8c49438
MUHAMMAD HUSSAIN <M.HUSSAIN7006@GMAIL.COM>
2022-04-12 09:05:18 +0500

    Assignment 4 (Boiler Plate)
    Initial React App

---
+++ Assignment 3/boiler-plate/src/App.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Jawan Pakistan</h1>
+        <h2>Assignment # 3</h2>
+        <h3>React App</h3>
+        <h5>Made By: Muhammad Hussain</h5>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
